a711feb94ed0eb4e1a0f8cdd6763c83d50ad123c
Shirahamah <shirahama.hiroki@runners.co.jp>
2022-12-19 15:56:47 +0900

    zustandのstore修正

--- src/libs/persist/store.ts
+++ src/libs/persist/store.ts
@@ -1,6 +1,5 @@
 import create from 'zustand'
-import { persist } from 'zustand/middleware'
-
+import { devtools, persist } from 'zustand/middleware'
 interface MyState {
   fishes: number
   addAFish: () => void
@@ -8,16 +7,18 @@ interface MyState {
 }
 
 export const useFishStore = create<MyState>()(
-  persist(
-    (set, get) => ({
-      fishes: 0,
-      addAFish: () => set({ fishes: get().fishes + 1 }),
-      resetAFish: () => set({ fishes: 0 })
-    }),
-    {
-      name: 'food-storage', // name of item in the storage (must be unique)
-      getStorage: () => sessionStorage, // (optional) by default the 'localStorage' is used
-      partialize: (state) => ({ fishes: state.fishes }),
-    }
+  devtools(
+    persist(
+      (set, get) => ({
+        fishes: 0,
+        addAFish: () => set((state) => ({fishes: get().fishes + 1}),false,'addAFish'),
+        resetAFish: () => set((state)=>({ fishes: 0 }),false,'resetAFish')
+      }),
+      {
+        name: 'food-storage', // name of item in the storage (must be unique)
+        getStorage: () => sessionStorage, // (optional) by default the 'localStorage' is used
+        partialize: (state) => ({ fishes: state.fishes }),
+      }
+    )
   )
 )
